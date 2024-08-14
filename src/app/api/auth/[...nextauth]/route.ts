c444c147be60f17192759c1e5ea37ce728dcbab8
bhautikT <bhautik.z@tridhyatech.com>
2024-08-14 10:38:52 +0530

    -add routes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,16 +6,15 @@ import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
   providers: [
-    GithubProvider({
-      clientId: "Ov23liFXF3H3vKwIM6Zy" as string,
-      clientSecret: "af9aa44266b5eb847e025747c94b81d751b4d8fd" as string,
-    }),
-    GoogleProvider({
-      clientId:
-        "164263697979-ghf3fhbgr8k59f9k0ipii1u142n5u9in.apps.googleusercontent.com" as string,
-      clientSecret: "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" as string,
-    }),
+    // GithubProvider({
+    //   clientId: process.env.Git_clientId as string,
+    //   clientSecret: process.env.Git_clientSecret as string,
+    // }),
+    // GoogleProvider({
+    //   clientId: process.env.Google_clientId as string,
+    //   clientSecret: process.env.Google_clientSecret as string,
+    // }),
   ],
 });
-//redirect google login url http://localhost:3000/api/auth/callback/google
+
 export { handler as GET, handler as POST };
